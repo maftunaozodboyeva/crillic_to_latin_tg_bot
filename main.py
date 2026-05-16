ba98893562768d896e79d96f45d97f30e2377b23
maftunaozodboyeva86@gmail.com <maftunaozodboyeva86>
2026-05-16 17:10:37 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# import telebot
+import telebot
+
+from transliterate import to_cyrillic, to_latin
+import transliterate
+
+TOKEN = "8499725923:AAHIBObKvA7w45OvV753N1FvK96pA_3SzqQ"
+
+bot = telebot.TeleBot(TOKEN, parse_mode=None)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Assalomu alaykum, botimizga hush kelibsiz! Sizga qanday yordam bera olaman?")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	text = message.text
+	if text.isascii():
+		bot.reply_to(message, to_cyrillic(text))
+	else:
+		bot.reply_to(message, to_latin(text))
+
+bot.infinity_polling()
+
+# s = input()
+# if s.isascii():
+#     print(to_cyrillic(s))
+# else:
+#     print(to_latin(s))
